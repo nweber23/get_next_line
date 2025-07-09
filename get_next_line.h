3bf58c170f077bee2877b093868bf91b0a34bd69
Niklas Weber <nweber@student.42heilbronn.de>
2025-07-09 11:12:44 +0200

    added missing ;

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 16:20:19 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/08 16:26:38 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/09 11:12:21 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
