22dc79d3be5a2be8cf0a5ccbe3e48f484b8279d7
Niklas Weber <nweber@student.42heilbronn.de>
2025-07-10 17:00:12 +0200

    find_nextline first implementation no test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 16:20:08 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/10 14:37:49 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/10 16:59:53 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,28 @@ static char	*line_get(char *leftover)
 
 static char	find_nextline(int fd, char *leftover)
 {
+	char	*buffer;
+	size_t	b_read;
 
+	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	b_read = 1;
+	while (!ft_strchr(leftover, '\n') && b_read > 0)
+	{
+		b_read = read(fd, buffer, BUFFER_SIZE);
+		if (b_read == -1)
+		{
+			free(buffer);
+			return (cleaner(&leftover));
+		}
+		buffer[b_read] = '\0';
+		leftover = ft_strjoin(leftover, buffer);
+		if (!leftover)
+			break ;
+	}
+	free(buffer);
+	return (leftover);
 }
 
 char	*get_next_line(int fd)
