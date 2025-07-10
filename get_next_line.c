35aecdc026f4fd9814b33fa63e67edd74f8412a0
Niklas Weber <nweber@student.42heilbronn.de>
2025-07-10 14:27:30 +0200

    startet work on main logic and created cleaner function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,36 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 16:20:08 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/08 16:20:10 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/10 14:26:41 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*cleaner(char **ptr)
+{
+	if (*ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+	}
+	return (NULL);
+}
+
+static char	*line_get(char *leftover)
+{
+
+}
+
+static char	find_nextline(int fd, char *leftover)
+{
+
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*leftover;
+
+	if (fd  < 0|| BUFFER_SIZE <= 0)
+		return (NULL);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 16:20:19 by nweber            #+#    #+#             */
-/*   Updated: 2025/07/09 11:12:21 by nweber           ###   ########.fr       */
+/*   Updated: 2025/07/10 13:26:12 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
